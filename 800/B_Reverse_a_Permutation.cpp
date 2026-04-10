64b2a53ceebe4234ce6d6885a9825f1eb719458a
Saumel-Das <saumel.spr@gmail.com>
2026-04-10 15:57:22 +0600

    solved

---
+++ 800/B_Reverse_a_Permutation.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a, b) memset(a, b, sizeof(a))
+#define check cout << "-__________________-" << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &u : v)
+        cin >> u;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] < n - i)
+        {
+            ll j;
+            for (j = i + 1; j < n; j++)
+            {
+                if (v[j] == n - i)
+                    break;
+            }
+            while(i < j)
+            {
+                swap(v[i++], v[j--]);
+            }
+            break;
+        }
+    }
+    for(auto u : v) cout << u << " ";
+    nl; 
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
